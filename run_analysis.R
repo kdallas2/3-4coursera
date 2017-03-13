0fb9e0388d71da9e98f6d742f917405b903c5c4b
kdallas2 <pcheng86@gmail.com>
2017-03-13 13:51:30 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
 library(data.table)
 
+# Load data
 path = getwd()
 Xtrain = fread(file.path(path, 'train', 'X_train.txt'))
 ytrain = fread(file.path(path, 'train', 'y_train.txt'))
@@ -8,15 +9,19 @@ ytest = fread(file.path(path, 'test', 'y_test.txt'))
 subtrain = fread(file.path(path,'train','subject_train.txt'))
 subtest = fread(file.path(path,'test','subject_test.txt'))
 
+# Concatenate train/test
 Xtotal = rbind(Xtrain,Xtest)
 ytotal = rbind(ytrain,ytest)
 subtotal = rbind(subtrain,subtest)
 
+#set col names
 setnames(subtotal, "V1", "Subject")
 setnames(ytotal, "V1", "Activity")
 
+#combine tables
 dt = cbind(subtotal,ytotal,Xtotal)
 
+#rename
 activ = fread(file.path(path,'activity_labels.txt'))
 features = fread(file.path(path,'features.txt'))
 collist = features[,V2]
@@ -27,6 +32,7 @@ for (i in 1:length(collist)){
 	names(dt)[i+2] = collist[i]
 }
 
+#select only std and mean, take mean of groups
 selection = c('std','mean','Activity','Subject')
 mask = grepl(paste(selection, collapse = '|'),names(dt))
 dt = dt[,..mask]	
